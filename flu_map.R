42c79446b942523e0d3efaa552585b00e3c70b07
Sara K. Satullo <sarasatullo@gmail.com>
2018-09-13 00:05:08 -0400

    first commit

---
+++ flu_map.R
@@ -0,0 +1,46 @@
+## import in CSV with total flu cases, create new DF with only total cases
+## get census population estimates for PA counties
+## join into one dataframe and calculate the flu rate per 100K
+
+setwd("~/Documents/Census_stuff")
+
+library(readr)
+library(dplyr)
+
+flu_18<- read_csv("~/Documents/Census_stuff/PA_final_flu_18.csv") 
+
+flu_18<- select(flu_18, County, Total_flu_cases) 
+
+##bring in census PA pop data
+
+install.packages("censusapi")
+library(censusapi)
+
+# Add key to .Renviron
+Sys.setenv(CENSUS_KEY="0274e47f16183ccd1ec42fb731164c8dfe5d0062")
+# Reload .Renviron
+readRenviron("~/.Renviron")
+# Check to see that the expected key is output in your R console
+Sys.getenv("CENSUS_KEY")
+
+
+apis <- listCensusApis()
+
+View(apis)
+
+pop_var <- listCensusMetadata(name= "pep/population", vintage = 2017,
+                               type= "variables")
+
+head(pop_var)
+
+pop_vars <- listCensusMetadata(name = "pep/population", vintage = 2017, 
+                                 type = "geography")
+head(pop_vars)
+
+
+
+pa_pop<- getCensus("pep/population",
+          vintage=2017,
+          vars=c("POP", "GEONAME"),
+          region="county:*",
+          regionin = "state:42")  
